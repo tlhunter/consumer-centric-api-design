4e6a940ee05fa0e28bfff649a4176a54219fe348
Thomas Hunter II <me@thomashunter.name>
2014-08-11 23:42:46 -0700

    build.sh cleanup

--- build.sh
+++ build.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
+# Running `./build --clean` will delete unwanted files and exit
 if [ "$1" == "--clean" ]; then
     rm -f book.{aux,bbl,blg,log,out,toc}
     exit
 fi
 
-# If the book doesn't get built multiple times,
-# the ToC ends up missing information.
-
+# If the book doesn't get built multiple times the ToC ends up missing information
 pdflatex book.tex
 bibtex book.aux
 pdflatex book.tex
 pdflatex book.tex
 
 # Renames "book.pdf" to something like "Consumer-Centric API Design v0.2.1.pdf"
-
 mv book.pdf Consumer-Centric\ API\ Design\ `git describe --abbrev=0`.pdf
 
-# run `./build.sh --perserve` if you want to keep these files around
+# Deletes messy files unless `./build.sh --perserve` flag has been added
 if [ "$1" != "--preserve" ]; then
     rm -f book.{aux,bbl,blg,log,out,toc}
 fi
